0b5ff63ba19fa9696cd5b266636df36b680b0d05
sebastian cardenas <scardenasga@unbosque.edu.co>
2025-04-07 20:02:04 -0500

    Creacion del proyecto

---
+++ src/test/java/co/edu/unbosque/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
